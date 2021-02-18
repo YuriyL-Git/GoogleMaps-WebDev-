345b079eef7b91d31ad55a24d6efcf8c5d3d0a25
Yuriy Lipchey <xaosmmm@gmail.com>
2021-02-18 10:10:39 +0200

    Setup current position

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
 const ACCESS_TOKEN = 'pk.eyJ1IjoibWFsYWs2IiwiYSI6ImNrbGFqOWpxMTB3Z2QydWxiMjEwamI0aHQifQ.wxx4aAalGtjlDgwbgVmMJg'
 
 
-var map = new mapboxgl.Map({
-    accessToken: ACCESS_TOKEN,
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11'
-});
+// var map = new mapboxgl.Map({
+//     accessToken: ACCESS_TOKEN,
+//     container: 'map',
+//     style: 'mapbox://styles/mapbox/streets-v11'
+// });
 
 navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {enableHighAccuracy: true})
 
+function errorLocation() {
+    console.log('Not possible to find current location')
+    setupMap([-2, -2])
+}
+
 function successLocation(position) {
-    setupMap(position.coords.longitude, position.coords.latitude)
+    setupMap([position.coords.longitude, position.coords.latitude])
     console.log(position)
 }
 
-function errorLocation() {
-    console.log('Location Error')
+function setupMap(centerPosition) {
+    const map = new mapboxgl.Map({
+        accessToken: ACCESS_TOKEN,
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: centerPosition,
+        zoom: 15
+    });
+
+    const navigationControls = new mapboxgl.NavigationControl()
+    map.addControl(navigationControls)
 }
